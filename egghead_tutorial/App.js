d82d0ec779257f9359fdbfd31593b32ebb2608e9
JupiterLikeThePlanet <jupiterbaudot@gmail.com>
2016-02-26 17:13:54 -0800

    Access child propertiesy

--- egghead_tutorial/App.js
+++ egghead_tutorial/App.js
@@ -139,50 +139,72 @@ import ReactDOM from 'react-dom';
 //refs are way to reference an instance of our component within our application
 //refs don't work with stateless function components
 
+// class App extends React.Component{
+//   constructor(){
+//     super();
+//     this.state = {
+//       red: 0,
+//       green:0,
+//       blue:0
+//     }
+//     this.update = this.update.bind(this)
+//   }
+// // this requires 'import ReactDOM from 'react-dom';'
+//   update(e){
+//     this.setState({
+//       red: ReactDOM.findDOMNode(this.refs.red.refs.inp).value,
+//       green: ReactDOM.findDOMNode(this.refs.green.refs.inp).value,
+//       blue: ReactDOM.findDOMNode(this.refs.blue.refs.inp).value
+//     })
+//   }
+//   render(){
+//     return(
+//       <div>
+//         <Slider ref="red" update={this.update} />
+//         {this.state.red}
+//         <br />
+//         <Slider ref="green" update={this.update} />
+//         {this.state.green}
+//         <br />
+//         <Slider ref="blue" update={this.update} />
+//         {this.state.blue}
+//         <br />
+//       </div>
+//     );
+//   }
+// }
+
+// class Slider extends React.Component {
+//   render(){
+//       return (
+//         <div >
+//           <input ref="inp" type="range"
+//             min="0"
+//             max="255"
+//             onChange={this.props.update} />
+//         </div>
+//     );
+//   }
+// }
+
+////////////////////////////////////////////////
+////8 accessing child properties////////////////
+
+//use this.props.children to access innerhtml or nested component of another component
+
 class App extends React.Component{
-  constructor(){
-    super();
-    this.state = {
-      red: 0,
-      green:0,
-      blue:0
-    }
-    this.update = this.update.bind(this)
-  }
-// this requires 'import ReactDOM from 'react-dom';'
-  update(e){
-    this.setState({
-      red: ReactDOM.findDOMNode(this.refs.red).value,
-      green: ReactDOM.findDOMNode(this.refs.green).value,
-      blue: ReactDOM.findDOMNode(this.refs.blue).value
-    })
-  }
   render(){
-    return(
-      <div>
-        <Slider ref="red" update={this.update} />
-        {this.state.red}
-        <br />
-        <Slider ref="green" update={this.update} />
-        {this.state.green}
-        <br />
-        <Slider ref="blue" update={this.update} />
-        {this.state.blue}
-        <br />
-      </div>
-    );
+    return <Button>I <Heart/> Login</Button>
   }
 }
 
-class Slider extends React.Component {
+class Button extends React.Component{
   render(){
-      return (
-        <input type="range"
-          min="0"
-          max="255"
-          onChange={this.props.update} />
-    );
+    return <button>{this.props.children}</button>
   }
 }
+
+const Heart = () => <span className="glyphicon glyphicon-heart"></span>
+
 
 export default App
